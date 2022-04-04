103f9d9dbedc9c614515c1d7038b3b4300a276b2
Amiko69 <amaralharbi74@gmail.com>
2022-04-04 20:15:54 +0300

    _

---
+++ Assets/controller.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class controller : MonoBehaviour
+{
+    float speed = .14f;
+    bool justJumped = false;
+    bool justMovedR = false;
+    bool justMovedL = false;    
+
+    CapsuleCollider2D collider;
+    public LayerMask groundLayer;
+    Rigidbody2D rb;
+    GameObject sphere;
+
+    void Awake()
+    {
+        sphere = GameObject.FindGameObjectWithTag("s");
+        rb = GetComponent<Rigidbody2D>();
+        collider = GetComponent<CapsuleCollider2D>();
+    }
+
+    void Update()
+    {
+        HandleInput();
+        Debug.DrawRay(collider.bounds.center, rb.velocity, Color.green);
+
+    }
+
+    private void FixedUpdate()
+    {
+        float xdif = -sphere.transform.position.x + transform.position.x;
+        float ydif = -sphere.transform.position.y + transform.position.y;
+        float angle = Mathf.Atan2(xdif, ydif) * Mathf.Rad2Deg;
+        transform.transform.rotation = Quaternion.Euler(0, 0, -angle );
+
+        rb.AddForce(-transform.up * 10);
+        print(rb.velocity);
+
+        if (justJumped)
+        {
+            rb.AddForce(transform.up * 8, ForceMode2D.Impulse);
+            justJumped = false;
+        }
+        if (justMovedR) { transform.position += transform.right * speed ; justMovedR = false; }
+        if (justMovedL) { transform.position += transform.right * -speed ; justMovedL = false; }
+
+    }
+    void HandleInput()
+    {
+        
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded())
+        {
+            justJumped = true;
+            
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            justMovedR = true;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            justMovedL = true;
+        }
+        
+        
+    }
+
+    private bool isGrounded()
+    {
+        RaycastHit2D raycast = Physics2D.Raycast(collider.bounds.center, -transform.up,(transform.localScale.y / 2 + .2f), groundLayer);
+
+        print(raycast.collider != null);
+        if (raycast.collider != null) return true;
+        else return false;
+    }
+}
---
+++ Assets/followPlayer.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class followPlayer : MonoBehaviour
+{
+    Transform playerT;
+    private void Awake()
+    {
+        playerT = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+    private void LateUpdate()
+    {
+        Vector3 temp = new Vector3(playerT.position.x, playerT.position.y, transform.position.z);
+        transform.position = temp;
+    }
+}
